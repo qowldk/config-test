53915e23c36fc3142444941863eeef310d9ab5b2
BaeJia <qowldk1030@gmail.com>
2024-01-28 20:00:54 +0900

    커스텀 환경 설정 파일 사용

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigObject } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { WeatherModule } from './weather/weather.module';
 
+console.log('env : ' + process.env.NODE_ENV);
+console.log('current working directory : ' + process.cwd());
+
+import config from './configs/config';
+
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), WeatherModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: `${process.cwd()}/envs/${process.env.NODE_ENV}.env`,
+      load: [config],
+    }),
+    WeatherModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
