81dcb99632137de33b3af6d7f2a62e057232204d
BaeJia <qowldk1030@gmail.com>
2024-02-01 02:53:36 +0900

    yaml 파일 환경변수

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -2,6 +2,8 @@ import common from './common';
 import local from './local';
 import dev from './dev';
 import prod from './prod';
+import { readFileSync } from 'fs';
+import * as yaml from 'js-yaml';
 
 const phase = process.env.NODE_ENV;
 
@@ -14,7 +16,12 @@ if (phase === 'local') {
   conf = prod;
 }
 
+const yamlConfig: Record<string, any> = yaml.load(
+  readFileSync(`${process.cwd()}/envs/config.yaml`, 'utf8'),
+);
+
 export default () => ({
   ...common,
   ...conf,
+  ...yamlConfig,
 });
